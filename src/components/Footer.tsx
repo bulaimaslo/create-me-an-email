3e818e471c2a487dd70634d392be5565c4cc527c
Adam Neumann <57113336+bulaimaslo@users.noreply.github.com>
2022-12-16 03:16:35 +0100

    Changed files structure

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+      <footer className="absolute bottom-5 w-full">
+        <h2 className="text-s text-center text-indigo-500 font-medium title-font m-auto">
+          Made by Adam Neumann with OpenAI
+        </h2>
+      </footer>
+    );
+}
